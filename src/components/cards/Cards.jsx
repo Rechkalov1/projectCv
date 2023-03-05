98b074b17c24c13fa24e26e87fec177fb8ab4685
EnVy <envy7321@gmail.com>
2023-03-05 11:07:34 +0200

    save

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import pic from "../img/pic.png"
 import face from "../img/face.png"
 import vector from "../img/Vector.svg"
+
 import "./style.css"
 function Cards() {
-  const [count, setCount] = useState(100500)
-  const [invited, setInvited] = useState(false)
+  const [count, setCount] = useState(() => {
+    return JSON.parse(window.localStorage.getItem("count")) ?? 100500
+  })
+  const [invited, setInvited] = useState(() => {
+    return JSON.parse(window.localStorage.getItem("invited")) ?? false
+  })
   const toggle = () => {
     setInvited(!invited)
     if (!invited) {
@@ -14,19 +19,23 @@ function Cards() {
       setCount(count - 1)
     }
   }
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count))
+    localStorage.setItem("invited", JSON.stringify(invited))
+  }, [count, invited])
 
   return (
-    <div className="cards">
-      <img className="vector" src={vector} alt="images" />
+    <div className="thumb_cards">
+      <img className="logo" src={vector} alt="images" />
       <img className="img" src={pic} alt="images" />
-      <div className="faceDiv">
-        <span className="span"></span>
+      <div className="thumb_img">
+        <span className="band_one"></span>
         <img className="faceImg" src={face} alt="images" />
-        <span className="span2"></span>
+        <span className="band_two"></span>
       </div>
-      <div className="info">
-        <p className="txt">777 TWEETS</p>
-        <p className="txt">{count} fOLLOWERS</p>
+      <div className="description">
+        <p className="txt_info">777 TWEETS</p>
+        <p className="txt_info">{count.toLocaleString("ja-JP")} fOLLOWERS</p>
 
         <button onClick={toggle} className={"button " + (invited ? "buttonIsActive" : "button")}>
           {invited ? "Following" : "Follow"}
